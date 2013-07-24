3e3279aef3385f2284007bc493e394b661a260e5
colo <colo@colo.e-ducativa.x>
2013-07-24 18:15:42 -0300

    Modified "charisma.js" to load examples with my nodejs roues

--- charisma/js/charisma.js
+++ charisma/js/charisma.js
@@ -18,7 +18,8 @@ $(document).ready(function(){
 	
 	function switch_theme(theme_name)
 	{
-		$('#bs-css').attr('href','public/charisma/css/bootstrap-'+theme_name+'.css');
+// 		$('#bs-css').attr('href','public/charisma/css/bootstrap-'+theme_name+'.css');
+		$('#bs-css').attr('href','css/bootstrap-'+theme_name+'.css');
 	}
 	
 	//ajax menu checkbox
